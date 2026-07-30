28bcc3e5b2139b93ebbd38d1adcfd9203972bfd0
Emanuel Ibarra <ibarra.emanuel.022@gmail.com>
2026-07-30 20:45:05 -0300

    Agregada logica y diseño del Solicitar tecnico
    revisar de todas maneras

--- src/app/components/cliente/cliente-dashboard/cliente-dashboard.ts
+++ src/app/components/cliente/cliente-dashboard/cliente-dashboard.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cliente-dashboard.html',
   styleUrl: './cliente-dashboard.css',
 })
---
+++ src/app/services/solicitud.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolicitudService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/solicitudes';
+
+  // Usamos la misma lógica que armaste en tu ProblemaService
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+  }
+
+  buscarTecnicos(problemasIds: number[]): Observable<any[]> {
+    return this.http.post<any[]>(
+      `${this.apiUrl}/buscar-tecnicos`, 
+      { problemasIds }, 
+      { headers: this.getHeaders() }
+    );
+  }
+
+  confirmarPago(pagoData: any): Observable<any> {
+    return this.http.post(
+      `${this.apiUrl}/confirmar-pago`, 
+      pagoData, 
+      { headers: this.getHeaders() }
+    );
+  }
+}
